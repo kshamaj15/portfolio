2ccb2dc1b095ed86a93a145a153a7d38fb6fc1f6
Kshama Jain <kshamaj150896@gmail.com>
2021-02-02 22:33:04 +0530

    about page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import logo from './logo.svg';
 import './App.scss';
-import pp from './assets/photo.jpg';
+import linkedin from './assets/linkedin.svg';
+import instagram from './assets/instagram.svg';
+import gmail from './assets/gmail.svg';
+import github from './assets/github.svg';
+// import linkedin from './assets/linkedin.svg';
 
 function App() {
   return (
@@ -17,10 +20,25 @@ function App() {
         </nav>
       </header>
       <main>
-        <div className="img-container"></div>
-        <div className="css-typing">
-          <p>I am a 2.5 years experienced Front end web developer with hands on skills</p>
-          <p>in React, Angular, Node JS, HTML5, CSS, SCSS, Bootstarp, JavaScript, jQuery.</p>
+        {/* <div className="css-typing">
+          <p>I am a 2.5 years experienced Front end web developer</p>
+          <p>with hands on skills in</p>
+          <p>React, Angular, Node JS, HTML5, CSS</p>
+          <p>SCSS, Bootstarp, JavaScript, jQuery.</p>
+        </div> */}
+        <div style={{display: 'flex'}}>
+          <div className="img-container"></div>
+          <div className="intro">
+            <p>Hi, I am Kshama Jain</p>
+            <p>Software Engineer</p>
+          </div>
+        </div>
+        <div className="links">
+          <span><img width="60px" src={linkedin}/></span>
+          <span><img width="60px" src={instagram}/></span>
+          <span><img width="60px" src={gmail}/></span>
+          <span><img width="60px" src={github}/></span>
+          <span><img width="60px" src={linkedin}/></span>
         </div>
       </main>
     </div>
