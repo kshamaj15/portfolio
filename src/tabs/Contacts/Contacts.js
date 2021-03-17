72b56da0e2527e4481db8cfea090db0ef0a94ac6
Kshama Jain <kshamaj150896@gmail.com>
2021-03-17 22:03:17 +0530

    contact tab added

---
+++ src/tabs/Contacts/Contacts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Contacts.scss';
+
+const Contacts = () => {
+    return (
+        <div className="tab mx-3 tab-contacts">
+            <h1>Contacts</h1>
+        </div>
+    )
+}
+
+export default Contacts;
