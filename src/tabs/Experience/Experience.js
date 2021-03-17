8cc0d43c2aff5c60179c6c0afc83c1e9dc87be41
Kshama Jain <kshamaj150896@gmail.com>
2021-03-17 09:34:59 +0530

    experience tab added

--- src/tabs/Experience/Experience.js
+++ src/tabs/Experience/Experience.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import Dew from '../../assets/dew.png';
+import Wipro from '../../assets/wipro.png';
+import './Experience.scss'; 
 
 const Experience = () => {
+    const [companies, setCompanies] = useState([{
+        name: 'Dew Solutions',
+        from: 'April 2020',
+        to: 'present',
+        logo: Dew
+    },{
+        name: 'Wipro',
+        from: 'July 2018',
+        to: 'March 2020',
+        logo: Wipro
+    }])
     return (
-        <>
-        </>
+        <div className="mx-3 tab tab-experience">
+            <h1>Experience</h1>
+            <div className="tab-experience-list">
+            {companies.map(company => {
+                return (
+                    <>
+                        <div className="tab-experience-list-item">
+                            <img src={Dew} height="100px" style={{borderRadius: '38px'}} alt="img"/>
+                            <p>{company.name}</p>
+                            <p>{company.from} - {company.to}</p>
+                        </div>
+                        
+                    </>
+                )
+            })}
+            </div>
+        </div>
     )
 }
 
